193fbcaa6b6567cf8aae9e03fbf3a3f0b1a06449
VVesterinen <54880100+VVesterinen@users.noreply.github.com>
2020-03-26 20:17:22 +0200

    test program

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Toimiiks tää?");
+        }
+    }
+}
