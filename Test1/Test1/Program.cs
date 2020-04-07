c07a9dce4c3aec2c0dec26891bde909a3246610e
VVesterinen <54880100+VVesterinen@users.noreply.github.com>
2020-04-07 11:38:48 +0300

    Update Program.cs

--- Test1/Test1/Program.cs
+++ Test1/Test1/Program.cs
@@ -6,7 +6,7 @@ namespace Test1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Toimiiks tää?");
+            Console.WriteLine("Moi");
         }
     }
 }
