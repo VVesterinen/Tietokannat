f414beffffc9c21c8e6c38b09c3d9a752cc87f3d
VVesterinen <54880100+VVesterinen@users.noreply.github.com>
2020-04-07 13:21:28 +0300

    new test program

--- Test1/Test1/Program.cs
+++ Test1/Test1/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Microsoft.EntityFrameworkCore;
+using Test1.Data;
 
 namespace Test1
 {
     class Program
     {
+        static readonly TestContext _testContext = new TestContext();
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
+           var persons =  _testContext.Test.ToList();
+           foreach (var p in persons)
+           {
+               Console.WriteLine($"Id: {p.Id}, Firstname: {p.FirstName}, Lastname: {p.LastName}");
+           }
+
+           var persons1 = _testContext.Test.Include(t => t.Test2).ToList();
+           foreach (var p in persons1)
+           {
+               Console.WriteLine($"Id: {p.Id}, Firstname: {p.FirstName}, Lastname: {p.LastName}");
+               foreach (var n in p.Test2)
+               {
+                   Console.WriteLine(n.Phone);
+               }
+           }
         }
     }
 }
